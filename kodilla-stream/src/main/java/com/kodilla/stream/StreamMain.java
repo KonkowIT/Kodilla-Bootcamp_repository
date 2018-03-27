47658129fcc330f8b47775f07943d3c63da72fc2
kconrado <konradkowalski95@gmail.com git config --global user.name kconradogit config --global user.email konradkowalski95@gmail.com>
2018-03-27 21:42:24 +0200

    Zadanie 7.2 - niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.beautifier.DecorateMethods;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecuter;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -26,8 +27,10 @@ public class StreamMain {
 
         poemBeautifier.beautify("House", DecorateMethods::addAtBegining);
         poemBeautifier.beautify("House", DecorateMethods::addAtEnd);
-        poemBeautifier.beautify("House", DecorateMethods::toUpperCase);
-        poemBeautifier.beautify("House", DecorateMethods::toLowerCase);
+        //poemBeautifier.beautify("House", DecorateMethods::toUpperCase);
+        //poemBeautifier.beautify("House", DecorateMethods::toLowerCase);
 
+        System.out.println("Using Stream to generate even numbers form 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
