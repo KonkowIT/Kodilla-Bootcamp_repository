a4b9e9d9cc8b076c19954451e595daf47d16ba36
kconrado <konradkowalski95@gmail.com git config --global user.name kconradogit config --global user.email konradkowalski95@gmail.com>
2018-03-27 22:49:23 +0200

    Zadanie 7.1 - Upiększacz tekstów

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/DecorateMethods.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/DecorateMethods.java
@@ -3,26 +3,22 @@ package com.kodilla.stream.beautifier;
 public class DecorateMethods{
 
     public static String addAtBegining(String toBeautify){
-
         String result = "ABC" + toBeautify;
         return result;
     }
 
     public static String addAtEnd(String toBeautify){
-
         String result = toBeautify + "ABC";
         return result;
     }
 
     public static String toUpperCase(String toBeautify){
-
-        String result = toBeautify.toUpperCase();
-        return result;
+        String resultToUpperCase = toBeautify.toUpperCase();
+        return resultToUpperCase;
     }
 
     public static String toLowerCase(String toBeautify){
-
-        String result = toBeautify.toLowerCase();
-        return result;
+        String resultToLowerCase = toBeautify.toLowerCase();
+        return resultToLowerCase;
     }
 }
