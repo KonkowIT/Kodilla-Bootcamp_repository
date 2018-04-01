78c3e8b35e66e02be0bdbb7d266206bf73e154ac
kconrado <konradkowalski95@gmail.com git config --global user.name kconradogit config --global user.email konradkowalski95@gmail.com>
2018-04-01 17:05:12 +0200

    Zadanie 7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,13 +1,12 @@
 package com.kodilla.stream.array;
 
 public class ArrayOperations {
-    /*static double getAverage(int[] numbers) {
-
-        int averageNumbers = getAverage(10).IntStream()
-                .range(numbers[0], numbers[9])
-                .map(System.out::println)
+    static double getAverage(int[] numbers) {
+        int averageNumbers = getAverage(numbers).IntStream()
+                .range(0, numbers.length)
+                .forEach(n -> numbers[n])
                 .average();
 
         return averageNumbers;
-    }*/
+    }
 }
