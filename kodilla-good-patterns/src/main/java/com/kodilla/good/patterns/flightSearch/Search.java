281b094cbcddd9032e47f254d329a441132798db
kconrado <konradkowalski95@gmail.com git config --global user.name kconradogit config --global user.email konradkowalski95@gmail.com>
2018-04-13 00:21:57 +0200

    update 9.5

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flightSearch/Search.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flightSearch/Search.java
@@ -1,39 +1,20 @@
+
 package com.kodilla.good.patterns.flightSearch;
-
-import java.util.Scanner;
-
-import static java.lang.System.in;
 
 public class Search {
     public static void main(String[] args) {
         SearchEngine searchEngine = new SearchEngine();
+        Airport warsaw = new Airport("Warsaw");
+        Airport poznan = new Airport("Poznan");
+        Airport wroclaw = new Airport("Wroclaw");
 
-        System.out.println("Please enter departure airport:\n(warsaw, gdansk, poznan, wroclaw, cracow)");
-        Scanner departure = new Scanner(in);
-        String departureAirport = departure.next().trim().toLowerCase();
-        if (departureAirport.equals("poznan") ||
-                departureAirport.equals("wroclaw") ||
-                departureAirport.equals("cracow") ||
-                departureAirport.equals("warsaw") ||
-                departureAirport.equals("gdansk")) {
+        System.out.println("Searching flights from Warsaw");
+        searchEngine.searchFrom(warsaw);
 
-            System.out.println("Please enter arrival airport:\n(warsaw, gdansk, poznan, wroclaw, cracow)");
-            Scanner arrival = new Scanner(in);
-            String arrivalAirport = arrival.next().trim().toLowerCase();
-            if (arrivalAirport.equals("poznan") ||
-                    arrivalAirport.equals("wroclaw") ||
-                    arrivalAirport.equals("cracow") ||
-                    arrivalAirport.equals("warsaw") ||
-                    arrivalAirport.equals("gdansk")) {
+        System.out.println("\nSearching flights to Warsaw");
+        searchEngine.searchTo(warsaw);
 
-                searchEngine.searching(departureAirport, arrivalAirport);
-            } else {
-                System.out.println("Input correct airport (warsaw, gdansk, poznan, wroclaw, cracow)");
-                System.exit(0);
-            }
-        } else {
-            System.out.println("Input correct airport (warsaw, gdansk, poznan, wroclaw, cracow)");
-            System.exit(0);
-        }
+        System.out.println("\nSearching connecting flight from Poznan to Wroclaw");
+        searchEngine.searchBetween(poznan, wroclaw);
     }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flightSearch/SearchEngine.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flightSearch/SearchEngine.java
@@ -1,83 +1,52 @@
 package com.kodilla.good.patterns.flightSearch;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-import static java.lang.System.in;
-
 public class SearchEngine {
-    public void searching(String departureAirport, String arrivalAirport) {
-        FlightDatabase flightDatabase = new FlightDatabase();
-        Random generator = new Random();
-        System.out.println("Searching flight " + departureAirport.toUpperCase() + " -> " + arrivalAirport.toUpperCase());
-        if(departureAirport.equals(arrivalAirport)) {
-            System.out.println("Departure airport must be different than arrival airport!");
-            System.exit(0);
-        } else {
-            Airport airportArrival = new Airport(arrivalAirport);
-            Airport airportDeparture = new Airport(departureAirport);
-            Flight flightTested = new Flight(airportArrival, airportDeparture);
-            List<Flight> flights = flightDatabase.flightSet.stream()
-                    .filter(a -> a.equals(flightTested))
-                    .collect(Collectors.toList());
+    public void searchFrom(Airport airport) {
+        List<Flight> flights = FlightDatabase.flightSet.stream()
+                .filter(a -> a.getDepartureAirport().equals(airport))
+                .collect(Collectors.toList());
 
-            if (flights.size() == 0) {
-                System.out.println("Searched flight doesn't exist. Do You want to search connecting flight?");
-                Scanner yOrN = new Scanner(in);
-                char answerScan = yOrN.next().trim().toLowerCase().charAt(0);
-                while (answerScan != 'y' && answerScan != 'n') {
-                    System.out.println("You need to enter your answer");
-                    Scanner yOrN2 = new Scanner(in);
-                    answerScan = yOrN2.next().trim().toLowerCase().charAt(0);
+        for (Flight flight : flights){
+            System.out.println("Flight from: " + flight.getDepartureAirport().getTown().toUpperCase() +
+                    " to " + flight.getArrivalAirport().getTown().toUpperCase());
+        }
+
+    }
+
+    public void searchTo(Airport airport) {
+        List<Flight> flights = FlightDatabase.flightSet.stream()
+                .filter(a -> a.getArrivalAirport().equals(airport))
+                .collect(Collectors.toList());
+
+        for (Flight flight : flights){
+            System.out.println("Flight from: " + flight.getDepartureAirport().getTown().toUpperCase() +
+            " to " + flight.getArrivalAirport().getTown().toUpperCase());
+        }
+
+    }
+
+    public void searchBetween(Airport airportDep, Airport airportArr) {
+        Map<Flight, Flight> connectingFlights = new HashMap<>();
+        List<Flight> connectingFlightsDep = FlightDatabase.flightSet.stream()
+                .filter(a -> (a.getDepartureAirport()).equals(airportDep))
+                .collect(Collectors.toList());
+
+        List<Flight> connectingFlightsArr = FlightDatabase.flightSet.stream()
+                .filter(a -> (a.getDepartureAirport()).equals(airportArr))
+                .collect(Collectors.toList());
+
+        for (int n = 0; n < connectingFlightsDep.size(); n++) {
+            for (int i = 0; i < connectingFlightsArr.size() || i < connectingFlightsDep.size(); i++) {
+                if (connectingFlightsDep.get(n).getDepartureAirport()
+                        .equals(connectingFlightsArr.get(i).getArrivalAirport())) {
+                    connectingFlights.put(connectingFlightsDep.get(n), connectingFlightsArr.get(i));
                 }
-
-                if (answerScan == 'y') {
-                    Map<Flight, Flight> connectingFlights = new HashMap<>();
-                    List<Flight> connectingFlightsDep = flightDatabase.flightSet.stream()
-                            .filter(a -> (a.getDepartureAirport().getTown()).equals(departureAirport))
-                            .collect(Collectors.toList());
-
-                    List<Flight> connectingFlightsArr = flightDatabase.flightSet.stream()
-                            .filter(a -> (a.getDepartureAirport()).equals(departureAirport))
-                            .collect(Collectors.toList());
-
-                    for (int n = 0; n < connectingFlightsDep.size(); n++) {
-                        for (int i = 0; i < connectingFlightsArr.size() || i < connectingFlightsDep.size(); i++) {
-                            if (connectingFlightsDep.get(n).getDepartureAirport()
-                                    .equals(connectingFlightsArr.get(i).getArrivalAirport())) {
-                                connectingFlights.put(connectingFlightsDep.get(n), connectingFlightsArr.get(i));
-                            }
-                        }
-                    }
-
-                    System.out.println("List of flights with connecting:");
-                    for (int a = 0; a < connectingFlights.size(); a++) {
-                        Set<Flight> keys = connectingFlights.keySet();
-                        for (Flight key : keys) {
-                            System.out.println(a + 1 + " flight: first flight: " + key.getDepartureAirport() + " -> "
-                                    + key.getArrivalAirport() + ", second flight: "
-                                    + connectingFlights.get(a).getDepartureAirport() + " ->"
-                                    + connectingFlights.get(a).getArrivalAirport());
-                        }
-                    }
-                    System.exit(0);
-
-                } else {
-                    System.out.println("Ok :( Bye");
-                    System.exit(0);
-                }
-
-            } else {
-                int hoursDecimal = generator.nextInt(3);
-                int hoursUnits = generator.nextInt(10);
-                int minutesDecimal = generator.nextInt(60);
-                int minutesUnits = generator.nextInt(60);
-                String time = hoursDecimal + hoursUnits + ":" + minutesDecimal + minutesUnits;
-                int flightNumber = generator.nextInt(100000);
-                System.out.println("Searched flight departure time: " + time + "flight number: " + flightNumber);
-                System.exit(0);
             }
         }
     }
 }
-
